a25943f025de6e303d6d3f101981ba8e2e2c23ac
Vladimir Mironiuk <vladimironiuk@gmail.com>
2021-06-12 15:04:02 +0300

    Add test to make sure that newly created RemoteMediaLoader object is not nil

---
+++ iTunes-Demo/MediaAPI/RemoteMediaLoader.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public final class RemoteMediaLoader: MediaLoader {
+
+    // MARK: - Lifecycle
+
+    public init() {
+    }
+
+    // MARK: - MediaLoader
+
+    public func load(from url: URL) {
+    }
+}
