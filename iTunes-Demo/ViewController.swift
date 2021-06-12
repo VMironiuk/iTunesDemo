5013c42ee29a2cd0fbe2b24a14f42ff799848a0f
Vladimir Mironiuk <vladimironiuk@gmail.com>
2021-06-12 11:43:05 +0300

    Fix SwiftLint warnings and errors

--- iTunes-Demo/ViewController.swift
+++ iTunes-Demo/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  iTunes-Demo
-//
-//  Created by Vladimir Mironiuk on 12.06.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
