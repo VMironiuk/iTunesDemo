90134a2fd0e2244bd9f212db28ac5adc43823ffe
Vladimir Mironjuk <vladimironiuk@gmail.com>
2021-09-11 15:00:55 +0300

    Add the new, empty yet project

---
+++ iTunesPreview/iTunesPreview.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for iTunesPreview.
+FOUNDATION_EXPORT double iTunesPreviewVersionNumber;
+
+//! Project version string for iTunesPreview.
+FOUNDATION_EXPORT const unsigned char iTunesPreviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iTunesPreview/PublicHeader.h>
+
+
