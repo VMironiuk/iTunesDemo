46a91773b915bb353121e7b422ebcff78a3881a5
Vladimir Mironjuk <vladimironiuk@gmail.com>
2022-01-29 18:23:19 +0200

    Add TrackItem

---
+++ iTunesPreview/TrackItem.swift
@@ -0,0 +1,17 @@
+//
+//  TrackItem.swift
+//  iTunesPreview
+//
+//  Created by Vladimir Mironiuk on 29.01.2022.
+//
+
+import Foundation
+
+struct TrackItem {
+    let trackName: String
+    let artistName: String
+    let collectionName: String
+    let artworkUrl100: URL?
+    let artworkUrl60: URL?
+    let previewURL: URL
+}
